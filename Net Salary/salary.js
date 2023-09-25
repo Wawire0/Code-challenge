b52533b2572727314a2b4b9bf34825f33fc17a42
sandra <sandra.wawire@student.moringaschool.com>
2023-09-25 14:55:38 +0300

    calculation code

--- Net Salary/salary.js
+++ Net Salary/salary.js
@@ -1,15 +1,15 @@
 
 function calculateDeductions(basicSalary, benefits) {
     const taxRates = {
-      low: 0.1,
-      medium: 0.15,
-      high: 0.3,
+      lowestRate: 0.1,
+      mediumRate: 0.30,
+      highRate: 0.35,
     };
   
     const nhifRates = {
-      low: 1500,
-      medium: 2000,
-      high: 2500,
+      lowestRate: 1000,
+      mediumRate: 2000,
+      highRate: 2500,
     };
   
     const nssfRate = 0.06;
@@ -20,21 +20,21 @@ function calculateDeductions(basicSalary, benefits) {
     let paye = 0;
   
     if (grossSalary <= 24000) {
-      paye = grossSalary * taxRates.low;
+      paye = grossSalary * taxRates.lowestRate;
     } else if (grossSalary <= 32333) {
-      paye = 2400 + (grossSalary - 24000) * taxRates.medium;
+      paye = 2400 + (grossSalary - 24000) * taxRates.mediumRate;
     } else {
-      paye = 2400 + 1649.95 + (grossSalary - 32333) * taxRates.high;
+      paye = 2400 + 1649.95 + (grossSalary - 32333) * taxRates.highRate;
     }
   
     let nhifDeduction = 0;
   
     if (grossSalary <= 5999) {
-      nhifDeduction = nhifRates.low;
+      nhifDeduction = nhifRates.lowestRate;
     } else if (grossSalary <= 7999) {
-      nhifDeduction = nhifRates.medium;
+      nhifDeduction = nhifRates.mediumRate;
     } else {
-      nhifDeduction = nhifRates.high;
+      nhifDeduction = nhifRates.highRate;
     }
   
     const nssfDeduction = grossSalary * nssfRate;
